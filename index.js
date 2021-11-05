8ab2da40c512c08560a9994516053b26da0a8292
Mike <mikewconroy3@gmail.com>
2021-11-05 13:53:44 -0500

    Assignment Complete

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+function saturdayFun (activity='roller-skate'){
+        return `This Saturday, I want to ${activity}!`
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
-}
+      };
+let mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+};
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+let wrapAdjective = function(special = "*") {
+  return function(theAdjective = "special") {
+    return `You are ${special}${theAdjective}${special}!`
   }
 }
+
